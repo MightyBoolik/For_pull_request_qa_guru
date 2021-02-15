9f47711e5fcba54c25987c82745e686fc9a27ac7
MightyBoolik <bkuchaev@gmail.com>
2021-02-16 01:05:56 +0500

    Create test repository

---
+++ src/test/java/pull_request/TestSelenideSearchingGoogle.java
@@ -0,0 +1,18 @@
+package pull_request;
+
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byName;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class TestSelenideSearchingGoogle {
+
+        @Test
+        void selenideSearchTest() {
+            open("https://www.google.com/");
+            $(byName("q")).setValue("Selenide").pressEnter();
+            $("#search").shouldHave(text("selenide.org"));
+        }
+}
